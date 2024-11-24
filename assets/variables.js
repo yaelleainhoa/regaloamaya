57e4f6df813e2e7e8cc1583c2e18ccf58543be9e
AmayaST <amayasaralegi@gmail.com>
2024-11-24 13:15:33 +0100

    changement ordre

--- assets/variables.js
+++ assets/variables.js
@@ -1,21 +1,23 @@
-export const games = [
-    ["maladie", "De quoi souffre ce patient ?"],
-    ["audio", "Reconnais-tu ce son ?"],
-    ["dcc", "Duo carré ou cash ?"],
+export const games = [ 
     ["vf", "Memory"],
+    ["dcc", "Duo carré ou cash ?"], 
     ["drag_and_drop", "Quelle saison ?"],
     ["exs", "Quel complice ?"],
     ["frise_cinema", "Quel acteur ?"],
-    ["frise_yt", "Quel youtubeur ?"],
-    ["frise", "Quel médecin ?"],
+    ["guess_image", "Qui porte cette tenue ?"], 
     ["guess_flag_mix", "On s'emmêle les drapeaux"],
+    ["maladie", "De quoi souffre ce patient ?"],
+    ["frise_yt", "Quel youtubeur ?"],
     ["guess_ia", "Quel enfant ?"],
-    ["guess_image", "Qui porte cette tenue ?"],
+    ["framed", "Framed like"], 
+    ["audio", "Reconnais-tu ce son ?"],
     ["intrus", "En quelle année ?"],
+    
     ["questions_indiv", "Questions à thème"],
     ["wiki", "Quel voix off"],
     ["fais_deviner", "Jeu de drapeau"],
-    ["framed", "Framed like"], 
+    ["frise", "Quel médecin ?"],
+    
    // dessin one piece yaelle
     
   
